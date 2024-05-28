48f5e92f433e390d13909844894715a1f8959191
MyreeYo <yoyotte.sahora@gmail.com>
2024-05-28 09:35:54 +0200

    Add password strength

--- INSCRIPTION/inscription.js
+++ INSCRIPTION/inscription.js
@@ -13,3 +13,26 @@ document
       // Vous pouvez ajouter du code ici pour envoyer les données du formulaire à un serveur
     }
   });
+
+document.getElementById("password").addEventListener("input", function () {
+  const password = document.getElementById("password").value;
+  const strengthElement = document.getElementById("password-strength");
+  const strength = getPasswordStrength(password);
+
+  strengthElement.textContent = `Force du mot de passe : ${strength.text}`;
+  strengthElement.className = `password-strength ${strength.class}`;
+});
+
+function getPasswordStrength(password) {
+  if (password.length < 6) {
+    return { text: "Faible", class: "weak" };
+  } else if (password.length >= 6 && /[\d\W]/.test(password)) {
+    if (password.length > 9) {
+      return { text: "Fort", class: "strong" };
+    } else {
+      return { text: "Moyen", class: "medium" };
+    }
+  } else {
+    return { text: "Faible", class: "weak" };
+  }
+}
