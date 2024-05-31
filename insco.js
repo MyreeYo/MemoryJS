e068bb5d410d90e792902f9a4351e1e24d5743b0
MyreeYo <yoyotte.sahora@gmail.com>
2024-05-31 09:55:43 +0200

    Commented

--- insco.js
+++ insco.js
@@ -1,40 +1,54 @@
+// Attends que le contenu du DOM soit entièrement chargé avant d'exécuter le code
 document.addEventListener("DOMContentLoaded", (event) => {
+  // Sélectionne l'élément d'entrée mdp et l'élément d'affichage de la force du mdp
   const passwordInput = document.getElementById("password");
   const passwordStrength = document.getElementById("passwordStrength");
 
+  // Ajoute un EventListener pour l'entrée mdp
   passwordInput.addEventListener("input", () => {
+    // Calcule la force mdp et màj affichage
     const strength = getPasswordStrength(passwordInput.value);
     displayPasswordStrength(strength);
   });
 
+  // Fonction pour calculer la force du mot de passe
   function getPasswordStrength(password) {
     let strength = 0;
+    // Incrémente force si mdp a au moins 6 caractères
     if (password.length >= 6) strength++;
+    // Incrémente force si mdp a au moins 9 caractères
     if (password.length >= 9) strength++;
+    // Incrémente force si mdp contient au moins un chiffre
     if (/\d/.test(password)) strength++;
+    // Incrémente force si mdp contient au moins un caractère spécial
     if (/[^\w\s]/.test(password)) strength++;
     return strength;
   }
 
+  // Affiche force mdp en fonction de sa valeur
   function displayPasswordStrength(strength) {
     switch (strength) {
       case 0:
       case 1:
+        // Affiche "Weak" et applique "weak"
         passwordStrength.textContent = "Weak";
         passwordStrength.className = "weak";
         break;
       case 2:
+        // Affiche "Medium" et applique CSS "medium"
         passwordStrength.textContent = "Medium";
         passwordStrength.className = "medium";
         break;
       case 3:
       case 4:
+        // Affiche "Strong" et applique CSS "strong"
         passwordStrength.textContent = "Strong";
         passwordStrength.className = "strong";
         break;
     }
   }
 
+  // Ajoute un EventListener pour soumettre formulaire inscription
   document
     .getElementById("signupForm")
     .addEventListener("submit", function (event) {
@@ -43,8 +57,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
       const email = document.getElementById("email").value;
       const password = document.getElementById("password").value;
 
-      // Check if username or email already exists
+      // Récupère utilisateurs existants du localStorage
       const users = JSON.parse(localStorage.getItem("users")) || [];
+      // Vérifie si pseudo ou l'email existe déjà
       const userExists = users.some(
         (user) => user.username === username || user.email === email
       );
@@ -53,7 +68,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
         return;
       }
 
-      // Check password strength
+      // Force mdp
       let passwordStrength = "weak";
       if (getPasswordStrength(password) === 2) {
         passwordStrength = "medium";
@@ -61,7 +76,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
         passwordStrength = "strong";
       }
 
-      // Save user to localStorage
+      // Crée un nouvel utilisateur et l'ajoute à la liste des utilisateurs
       const newUser = {
         username: username,
         email: email,
@@ -71,13 +86,14 @@ document.addEventListener("DOMContentLoaded", (event) => {
       users.push(newUser);
       localStorage.setItem("users", JSON.stringify(users));
 
-      // Automatically login user
+      // Enregistre utilisateur actuel dans localStorage pour une co auto
       localStorage.setItem("currentUser", JSON.stringify(newUser));
 
-      // Redirect to homepage
+      // Redirige vers la page d'accueil
       window.location.href = "homepage.html";
     });
 
+  // Ajoute EventListener pour soumettre formulaire co
   document
     .getElementById("loginForm")
     .addEventListener("submit", function (event) {
@@ -85,14 +101,17 @@ document.addEventListener("DOMContentLoaded", (event) => {
       const loginUsername = document.getElementById("loginUsername").value;
       const loginPassword = document.getElementById("loginPassword").value;
 
-      // Check if user exists and password is correct
+      // Récupère les utilisateurs existants du localStorage
       const users = JSON.parse(localStorage.getItem("users")) || [];
+      // Vérifie si l'utilisateur existe et si le mdp est correct
       const currentUser = users.find(
         (user) =>
           user.username === loginUsername && user.password === loginPassword
       );
       if (currentUser) {
+        // Enregistre utilisateur dans localStorage et co auto
         localStorage.setItem("currentUser", JSON.stringify(currentUser));
+        // Redirige page d'accueil
         window.location.href = "homepage.html";
       } else {
         alert("Invalid username or password!");
--- jeu.js
+++ jeu.js
@@ -1,3 +1,4 @@
+// Tableau de cartes avec paires
 const cardsArray = [
   { name: "card1", img: "https://zupimages.net/up/24/22/opkz.png" },
   { name: "card2", img: "https://zupimages.net/up/24/22/0udz.png" },
@@ -17,9 +18,11 @@ const cardsArray = [
   { name: "card8", img: "https://zupimages.net/up/24/22/afon.png" },
 ];
 
+// Sélectionne l'élément du jeu de mémoire et le son de paire
 const game = document.querySelector(".memory-game");
 const pairSound = document.getElementById("pair-sound");
 
+// Fonction pour créer une carte
 const createCard = (card) => {
   const cardElement = document.createElement("div");
   cardElement.classList.add("memory-card");
@@ -40,72 +43,86 @@ const createCard = (card) => {
   return cardElement;
 };
 
+// Fonction pour initialiser le jeu
 const initializeGame = () => {
-  game.innerHTML = "";
+  game.innerHTML = ""; // Vide le contenu du jeu
+  // Mélange les cartes de manière aléatoire
   const shuffledCards = cardsArray.toSorted(() => 0.5 - Math.random());
+  // Pour chaque carte, crée un élément de carte et l'ajoute au jeu
   shuffledCards.forEach((card) => {
     const cardElement = createCard(card);
     game.appendChild(cardElement);
   });
 
+  // Ajoute un écouteur d'événements de clic à chaque carte
   const cards = document.querySelectorAll(".memory-card");
   cards.forEach((card) => card.addEventListener("click", flipCard));
 };
 
-let hasFlippedCard = false;
-let lockBoard = false;
-let firstCard, secondCard;
-let matchedPairs = 0;
+// Variables pour gérer l'état du jeu
+let hasFlippedCard = false; // Indique si une carte a été retournée
+let lockBoard = false; // Verrouille plateau pour empêcher de retourner plus de deux cartes
+let firstCard, secondCard; // Réf aux deux cartes retournées
+let matchedPairs = 0; // Compte le nombre de paires trouvées
 
+// Fonction pour retourner une carte
 const flipCard = function () {
-  if (lockBoard) return;
-  if (this === firstCard) return;
+  if (lockBoard) return; // Ne fait rien si le plateau est verrouillé
+  if (this === firstCard) return; // Ne fait rien si la même carte est cliquée deux fois
 
-  this.classList.add("flip");
+  this.classList.add("flip"); // Ajoute flip pour retourner carte
 
   if (!hasFlippedCard) {
+    // Si aucune carte n'a été retournée
     hasFlippedCard = true;
     firstCard = this;
     return;
   }
 
-  secondCard = this;
-  checkForMatch();
+  secondCard = this; // Assigne la deuxième carte
+  checkForMatch(); // Vérifie si les deux cartes correspondent
 };
 
+// Fonction pour vérifier si les deux cartes correspondent
 const checkForMatch = () => {
-  const isMatch = firstCard.dataset.name === secondCard.dataset.name;
-  isMatch ? disableCards() : unflipCards();
+  const isMatch = firstCard.dataset.name === secondCard.dataset.name; // Compare les noms des cartes
+  isMatch ? disableCards() : unflipCards(); // Si paire, désactive les cartes, sinon retourne les cartes
   if (isMatch) {
-    pairSound.play();
-    matchedPairs++;
+    pairSound.play(); // Joue le son de paire
+    pairSound.volume = 0.2;
+    matchedPairs++; // Incrémente nbr paires trouvées
   }
 };
 
+// Fonction désactiver carte ap paire
 const disableCards = () => {
-  firstCard.removeEventListener("click", flipCard);
-  secondCard.removeEventListener("click", flipCard);
-  resetBoard();
+  firstCard.removeEventListener("click", flipCard); // Enlève EventListener clic
+  secondCard.removeEventListener("click", flipCard); // Enlève EventListener clic
+  resetBoard(); // Réinitialise état plateau
 };
 
+// Fonction pour retourner les cartes si pas pair
 const unflipCards = () => {
-  lockBoard = true;
+  lockBoard = true; // Verrouille plateau
   setTimeout(() => {
-    firstCard.classList.remove("flip");
-    secondCard.classList.remove("flip");
-    resetBoard();
-  }, 1500);
+    firstCard.classList.remove("flip"); // Retire flip
+    secondCard.classList.remove("flip"); // Retire flip
+    resetBoard(); // Réinitialise plateau
+  }, 1500); // Attendre 1.5 secondes avant de retourner les cartes
 };
 
+// Fonction pour réinitialiser les variables état plateau
 const resetBoard = () => {
-  [hasFlippedCard, lockBoard] = [false, false];
-  [firstCard, secondCard] = [null, null];
+  [hasFlippedCard, lockBoard] = [false, false]; // Réinitialise les variables
+  [firstCard, secondCard] = [null, null]; // Réinitialise les cartes
 };
 
+// Ajoute EventListener pour réinitialiser jeu avec spacebar
 document.addEventListener("keydown", (event) => {
   if (event.code === "Space") {
     initializeGame();
   }
 });
 
+// Initialise je au chargmt page
 initializeGame();
